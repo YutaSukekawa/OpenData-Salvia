09e7e6c196d1082fc3d523689887feb4d70dfa1a
yutasukekawa <yutasukekawa@gmail.com>
2017-12-09 22:36:37 +0900

    up to date

--- js/site-init.js
+++ js/site-init.js
@@ -1,14 +1,16 @@
 /* サイトの初期化 riot tag のマウント後, 呼び出す */
 function siteInit() {
 
+    /* ページ読み込み確認をして */
     const promise = new Promise((resolve, reject) => {
         setInterval(() => {
-            if(document.getElementById('header').clientHeight != null){
+            if(document.getElementById('footer').clientHeight != null){
                 resolve();
             }
         }, 100);
     });
 
+    /* start 画面の実行 */
     promise.then(() => {
 
         /* start 画面の表示 */
@@ -27,31 +29,58 @@ function siteInit() {
         osm_window.style.borderRadius = 0;
         osm_window.style.backgroundColor = 'rgba(33, 33, 33, 0.4)';
 
-        /* スタートページ文字の style, class 関連 */
+        /* スタートページ文字の style, class 関連を文字列として格納 */
         const start_div_style = 'width:100%;' +
-                                'height: 100%;' +
-                                'position: relative;' +
-                                'display: table;';
+                                'height: 100%;';
 
-        const start_ptag_style = 'display: table-cell;' +
-                                 'vertical-align: middle;' +
+        const start_ptag_style = 'display: none;' +
+                                 'position: relative;' +
+                                 'top: 50%;' +
+                                 '-webkit-transform: translateY(-50%); /* Safari用 */' +
+                                 'transform: translateY(-50%);' +
                                  'font-family: \'Mplus 1p\';' +
                                  'font-size: 4vw;' +
                                  'cursor: default;';
 
         const start_ptag_class = 'text-center text-white';
 
-        const start_str = 'オープンデータで<br>見つけよう！';
+        const start_str = '"Open Data" で<br>見つけよう！';
 
+        /* 格納した文字列を HTML に */
         const start_HTML =  '<div id="start-div" style="' + start_div_style + '">' +
-                                '<p style="' + start_ptag_style + '" class="' + start_ptag_class + '">' + start_str + '</p>' +
+                                '<p id="start_str" style="' + start_ptag_style + '" class="' + start_ptag_class + '">' + start_str + '</p>' +
                             '</div>';
 
-
-        /* 文字の表示 */
+        /* innerHTML で要素を追加 */
         osm_window.innerHTML = start_HTML;
+
+        /* <p> タグ id start_str の上下センタリング */
+        var start_div = document.getElementById('start_div');
+        var start_ptag = document.getElementById('start_str');
+
+        /* 文字をフェードインで表示 */
+        $(start_ptag).fadeIn(1500);
+
+        /* フェードアウトしたら */
+        const promise = new Promise((resolve, reject) => {
+
+            setTimeout(function() {
+
+                $(start_ptag).fadeOut(1500);
+
+                setTimeout(function() {
+                    resolve();
+                }, 1600);
+
+            }, 3000);
+
+        });
+
+        /* <p> タグ id start_str を削除 */
+        promise.then(() => {
+            start_ptag.remove();
+        });
+
     });
-
-
 
 }
