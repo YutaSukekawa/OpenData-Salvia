aa3b1b43ea6d6405a544a4bbe7c9d7066f03679b
yutasukekawa <yutasukekawa@gmail.com>
2017-12-09 21:48:26 +0900

    up to date

--- js/site-init.js
+++ js/site-init.js
@@ -1,46 +1,57 @@
 /* サイトの初期化 riot tag のマウント後, 呼び出す */
 function siteInit() {
 
-    alert(document.getElementById('header').clientHeight);
+    const promise = new Promise((resolve, reject) => {
+        setInterval(() => {
+            if(document.getElementById('header').clientHeight != null){
+                resolve();
+            }
+        }, 100);
+    });
 
-    /* start 画面の表示 */
-    var header_height = document.getElementById('header').clientHeight;
-    var footer_rect = document.getElementById('footer').getBoundingClientRect();
-    var main_height = footer_rect.top - header_height;
-    var osm_window = document.getElementById('map');
+    promise.then(() => {
 
-    osm_window.style.height = '50vh';
+        /* start 画面の表示 */
+        var header_height = document.getElementById('header').clientHeight;
+        var footer_rect = document.getElementById('footer').getBoundingClientRect();
+        var main_height = footer_rect.top - header_height;
+        var osm_window = document.getElementById('map');
 
-    var oc_margin_top = Math.floor((main_height - osm_window.clientHeight) / 2);
-    osm_window.style.marginTop = oc_margin_top + 'px';
+        osm_window.style.height = '50vh';
 
-    /* スタート画面用に要素のスタイルを書き換え */
-    osm_window.style.width = '100%';
-    osm_window.style.borderRadius = 0;
-    osm_window.style.backgroundColor = 'rgba(33, 33, 33, 0.4)';
+        var oc_margin_top = Math.floor((main_height - osm_window.clientHeight) / 2);
+        osm_window.style.marginTop = oc_margin_top + 'px';
 
-    /* スタートページ文字の style, class 関連 */
-    const start_div_style = 'width:100%;' +
-                            'height: 100%;' +
-                            'position: relative;' +
-                            'display: table;';
+        /* スタート画面用に要素のスタイルを書き換え */
+        osm_window.style.width = '100%';
+        osm_window.style.borderRadius = 0;
+        osm_window.style.backgroundColor = 'rgba(33, 33, 33, 0.4)';
 
-    const start_ptag_style = 'display: table-cell;' +
-                             'vertical-align: middle;' +
-                             'font-family: \'Mplus 1p\';' +
-                             'font-size: 4vw;' +
-                             'cursor: default;';
+        /* スタートページ文字の style, class 関連 */
+        const start_div_style = 'width:100%;' +
+                                'height: 100%;' +
+                                'position: relative;' +
+                                'display: table;';
 
-    const start_ptag_class = 'text-center text-white';
+        const start_ptag_style = 'display: table-cell;' +
+                                 'vertical-align: middle;' +
+                                 'font-family: \'Mplus 1p\';' +
+                                 'font-size: 4vw;' +
+                                 'cursor: default;';
 
-    const start_str = 'オープンデータで<br>見つけよう！';
+        const start_ptag_class = 'text-center text-white';
 
-    const start_HTML =  '<div id="start-div" style="' + start_div_style + '">' +
-                            '<p style="' + start_ptag_style + '" class="' + start_ptag_class + '">' + start_str + '</p>' +
-                        '</div>';
+        const start_str = 'オープンデータで<br>見つけよう！';
+
+        const start_HTML =  '<div id="start-div" style="' + start_div_style + '">' +
+                                '<p style="' + start_ptag_style + '" class="' + start_ptag_class + '">' + start_str + '</p>' +
+                            '</div>';
 
 
-    /* 文字の表示 */
-    osm_window.innerHTML = start_HTML;
+        /* 文字の表示 */
+        osm_window.innerHTML = start_HTML;
+    });
+
+
 
 }
