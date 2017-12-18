2a84ad8a1d1e8ec1664995bfb6a8159388116f70
yutasukekawa <yutasukekawa@gmail.com>
2017-12-18 20:26:45 +0900

    up to date

--- js/menu-item-onclick.js
+++ js/menu-item-onclick.js
@@ -91,7 +91,7 @@ class MenuItem {
                     osm_window.style.marginTop = oc_margin_top + 'px';
                     osm_window.style.left = '15%';
 
-                    if(browser == 'safari') div_cf.style.marginTop = oc_margin_top + 'px';
+                    if (browser == 'safari') oc_margin_top = 0;
 
                     /* menu のスタイルを変更 */
                     var contents_find_menu = document.getElementById('menu');
