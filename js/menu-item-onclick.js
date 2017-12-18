942fcbb2fd14548a3ad24ff30af541c413c49acd
yutasukekawa <yutasukekawa@gmail.com>
2017-12-18 20:19:06 +0900

    up to date

--- js/menu-item-onclick.js
+++ js/menu-item-onclick.js
@@ -72,7 +72,7 @@ class MenuItem {
 
                     var div_cf = document.getElementById('div-cf');
                     div_cf.style.marginTop = cf_margin_top + 'px';
-                    if(browser == 'safari') div_cf.style.marginTop = (cf_margin_top - 20) + 'px';
+                    if(browser == 'safari') div_cf.style.marginTop = 0;
                     div_cf.style.height = document.getElementById('container').clientHeight + 'px';
 
                     /* もし, divタグ start-div が存在したら削除 */
